bcac156782b8ad36f8a2db8c731306c003fe0daa
Koen Klinkers <k.klinkers@gmail.com>
2024-06-20 22:37:40 +0200

    more

--- src/app.rs
+++ src/app.rs
@@ -33,29 +33,28 @@ impl App {
     }
 
     pub fn header(&self) -> impl Widget {
-        Paragraph::new("BigBrainWordle 󰧑 ")
-            .centered()
-            .style(Style::default().fg(Color::Green))
+        Paragraph::new("BigBrainWordle 󰧑 ").style(Style::default().fg(Color::Green))
     }
 
     pub fn instuctions(&self) -> impl Widget {
-        Paragraph::new("Lets start with tares").centered()
+        Paragraph::new("Lets start with tares")
     }
 
     pub fn board(&self) -> impl Widget {
-        Paragraph::new("▫️▫️▫️▫️▫️\n▫️▫️▫️▫️▫️\n▫️▫️▫️▫️▫️\n▫️▫️▫️▫️▫️\n▫️▫️▫️▫️▫️\n▫️▫️▫️▫️▫️").centered()
+        Paragraph::new("▫️▫️▫️▫️▫️\n▫️▫️▫️▫️▫️\n▫️▫️▫️▫️▫️\n▫️▫️▫️▫️▫️\n▫️▫️▫️▫️▫️\n▫️▫️▫️▫️▫️")
     }
     pub fn draw(&self, f: &mut Frame) {
         let layout = Layout::default()
             .direction(Direction::Vertical)
-            .constraints(vec![
-                Constraint::Length(1),
-                Constraint::Length(3),
-                Constraint::Min(5),
-            ])
+            .constraints(vec![Constraint::Length(2), Constraint::Length(8)])
             .split(f.size());
+
+        let body = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints(vec![Constraint::Length(15), Constraint::Length(30)])
+            .split(layout[1]);
         f.render_widget(self.header(), layout[0]);
-        f.render_widget(self.instuctions(), layout[1]);
-        f.render_widget(self.board(), layout[2]);
+        f.render_widget(self.instuctions(), body[1]);
+        f.render_widget(self.board(), body[0]);
     }
 }
